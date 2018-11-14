3053560cf579900f1a206794a612c549987134f5
Sheep <Sheepsaurus@gmail.com>
2018-11-14 13:26:24 +0100

    Final Commit

--- WasteScanProtoType/MainWindow.xaml.cs
+++ WasteScanProtoType/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,24 @@ namespace WasteScanProtoType
 		public MainWindow()
 		{
 			InitializeComponent();
-		}		
-	}
+
+			TextHandle textHandle = new TextHandle
+			{
+				SaveAmount = "SAVE 20,-",
+				InformationFact = "Did you know that 40% of all food is wasted in the US?\n" + 
+								  "1.3 billion tons of food are wasted every year.\n" + 
+								  "This amounts to US$1 trillion dollars of wasted or lost food.\n" + 
+								  "If wasted food was a country, " + 
+								  "it would be the third largest producer of carbon dioxide in the world, " + 
+								  "around 3.3 billions tons of carbon dioxide.",
+				BarcodeWidth = 300,
+				BarcodeHeight = 170
+			};
+
+			textHandle.LineLength = textHandle.BarcodeWidth - 3;
+			textHandle.LineOffset = textHandle.BarcodeHeight / 2;			
+
+			DataContext = textHandle;
+		}
+	}	
 }
---
+++ WasteScanProtoType/TextHandle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasteScanProtoType
+{
+	public class TextHandle
+	{	
+		public string SaveAmount { get; set; }
+		public string InformationFact { get; set; }		
+		public int LineLength { get; set; }
+		public int LineOffset { get; set; }
+		public int BarcodeWidth { get; set; }
+		public int BarcodeHeight { get; set; }
+	}
+}
